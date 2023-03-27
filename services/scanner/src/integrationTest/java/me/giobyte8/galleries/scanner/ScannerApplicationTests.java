d5edc1eedcab238717434a24bda64cbc3365c989
Giovanni Aguirre <giovanni.fi05@gmail.com>
2023-03-26 22:31:39 -0600

    Add simple assertion to verify context loaded

--- services/scanner/src/integrationTest/java/me/giobyte8/galleries/scanner/ScannerApplicationTests.java
+++ services/scanner/src/integrationTest/java/me/giobyte8/galleries/scanner/ScannerApplicationTests.java
@@ -1,16 +1,24 @@
 package me.giobyte8.galleries.scanner;
 
 import me.giobyte8.galleries.scanner.amqp.ScanRequestsListener;
+import me.giobyte8.galleries.scanner.services.MediaScannerService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
 @MockBean(ScanRequestsListener.class)
 class ScannerApplicationTests {
 
+    @Autowired
+    private MediaScannerService mScannerSvc;
+
     @Test
     void contextLoads() {
+        assertThat(mScannerSvc).isNotNull();
     }
 }
