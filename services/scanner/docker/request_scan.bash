a72a772f41ece3f16d3c49c0d39e72c3be0f8d69
Giovanni Aguirre <giovanni.fi05@gmail.com>
2024-06-15 22:01:32 -0600

    Update message payload and key binding

--- services/scanner/docker/request_scan.bash
+++ services/scanner/docker/request_scan.bash
@@ -7,19 +7,19 @@ function json_escape() {
 
 # ref: https://stackoverflow.com/a/4774063/3211029
 HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-CURR_PATH="${pwd}"
-cd $HERE
+CURP="${pwd}"
+cd "$HERE"
 
 source scanner.env
 RABBITMQ_API_PORT=15672
 RABBITMQ_EXCHANGE=GL_EXCHANGE
-RABBITMQ_ROUTING_KEY=GL_SCAN_ORDERS
+RABBITMQ_ROUTING_KEY=GL_SCAN_REQUESTS
 UUID=$(uuidgen)
 
 msg="{
   \"id\": \"$UUID\",
-  \"dirHPath\": \"ae44596c94bfd98471dd2da440b699d6cfd1570f4f03b9c55acedfeb5ca71fdc\",
-  \"requestedAt\": \"2050-02-14\"
+  \"dirPath\": \"testphotos\",
+  \"requestedAt\": \"2050-02-14T10:00:00\"
 }"
 j_msg=$(json_escape "$msg")
 
@@ -37,3 +37,5 @@ curl -s \
   -X POST                                     \
   -d "$amqp_msg"                              \
   http://localhost:$RABBITMQ_API_PORT/api/exchanges/%2F/$RABBITMQ_EXCHANGE/publish
+
+cd "$CURP"
